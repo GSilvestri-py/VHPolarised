563111a10d7652ba90458a9864dd03ffb621fdde
GSilvestri-py <gsilvestri13@campus.unimib.it>
2025-12-05 11:36:36 +0100

    added b tagging ptype

--- Code/Gen/NanoRead/clean_jets.py
+++ Code/Gen/NanoRead/clean_jets.py
@@ -19,6 +19,21 @@ def compute_deltaR(vec1, vec2):
 
     return np.sqrt( (deltaphi)**2 + (deltaeta)**2 )
 
+
+def is_b_hadron(pdgId):                     #return True if b hadron in event -> if b in event
+    b_hadrons = [
+                    511,   # B0
+                    521,   # B+
+                    531,   # Bs0
+                    541,   # Bc+
+                    5122,  # Lambda_b0
+                    5132,  # Xi_b-
+                    5232,  # Xi_b0
+                    5332   # Omega_b-
+                    ]
+    return abs(pdgId) in b_hadrons
+
+
 def main():
 
     filename = "/gwpool/users/gisilvestri/VHPolar/RECO/nanoLatino_ZH_H_ZToLL_ZL__part0.root"
@@ -51,16 +66,17 @@ def main():
         VisTau_phi    = tree["GenVisTau_phi"].array(library="ak")
         VisTau_mass   = tree["GenVisTau_mass"].array(library="ak")
         VisTau_mother = tree["GenVisTau_genPartIdxMother"].array(library="ak")
-        VisTau_status = tree["GenVisTau_status"].array(library="ak")
+        VisTau_status = tree["GenVisTau_status"].array(library="ak")      
         
 
-        for i in range(10):                                                             #loop over events
+        for i in range(130):                                                             #loop over events
 
             print(f"=============================================== EVENT {i} ===============================================")
 
             index_list = []                                                        
             e_counter = 0
             mu_counter = 0
+            is_b_inevent = False
 
             #LHE array i component
             n_lhe_evt      = n_lhe[i]
@@ -115,6 +131,13 @@ def main():
                 pid = int(pdg_evt[n])
                 mother = mother_pdg(n)
 
+                if abs(pid) == 15:
+                    print(f"tau in event with mother {mother}")
+
+                if is_b_hadron(abs(pid)):
+                    is_b_inevent = True
+                    print(f"quark b in event")
+
                 if pid == 25:
                     child_indices = [k for k in range(len(pdg_evt)) if int(midx_evt[k]) == n]
 
@@ -154,7 +177,7 @@ def main():
                             print(f"jet with index {j} is the mu+/- jet: mu j deltaR = {deltaR_muj}")
                             index_list.append(j)
             
-            
+           
             #clean tau jets from GenJet
             for j_t in range(n_jets_vistau_evt):
                 j_t_vec = build_4vec_from_pt_eta_phi_m(VisTau_pt_evt[j_t], VisTau_eta_evt[j_t],
@@ -197,9 +220,18 @@ def main():
             print(f"GJet_mass = {GJet_mass_evt}")
             print(f"GJet_h_flav = {GJet_h_flav_evt}")
 
-
+            print(f"n_jets_vistau_evt = {n_jets_vistau_evt}")
+            print(f"VisTau_pt_evt = {VisTau_pt_evt}")
+            print(f"VisTau_eta_evt = {VisTau_eta_evt}")
+            print(f"VisTau_phi_evt = {VisTau_phi_evt}")
+            print(f"VisTau_mass_evt = {VisTau_mass_evt}")
+            print(f"VisTau_mother_evt = {VisTau_mother_evt}")
+            print(f"VisTau_status_evt = {VisTau_status_evt}")
+           
             print(f"\n------------------------------------------ b jets ------------------------------------------")
+            
             #identify b quark jets
+            jet_candidates = 0
             for e1 in range(n_jets_evt-1):                          #njets - 1 avoids duplicate combinations
 
                 if GJet_pt_evt[e1] == 0:    continue                #avoid 'cleaned' jets
@@ -208,11 +240,9 @@ def main():
                                                                GJet_phi_evt[e1], GJet_mass_evt[e1])
                     
                 for e2 in range(n_jets_evt):
-                    
                     if e2 <= e1:    continue                         #avoid duplicate pairs
 
                     print(f"jet pair indices = ({e1}, {e2})")
-
                     j2_vec = build_4vec_from_pt_eta_phi_m(GJet_pt_evt[e2], GJet_eta_evt[e2], 
                                                                GJet_phi_evt[e2], GJet_mass_evt[e2])
 
@@ -221,7 +251,18 @@ def main():
                     print(f"jj invariant mass = {mass_jj}")
                     tollerance = 15
                     if abs(mass_jj - 125) < tollerance:
+                        jet_candidates += 1
                         print(f"> possible jet {e1}-{e2} from h decay b pair\n")
+
+            b_from_h = False
+            if jet_candidates > 0:
+                b_from_h = True
+
+            if b_from_h and not is_b_inevent:
+                print(f"jets with expected invariant mass but no b in event")
+
+            if is_b_inevent and not b_from_h:
+                print(f"b in event but no candidate h > bb jets")
 
             print(f"\n------------------------------------------------------------------------------------------------------------------------------------------\n")
 
